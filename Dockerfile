f9fcaf2d1f2c6ac85298b3cc7e90bd2d34669b93
Ken Gilmer <kgilmer@gmail.com>
2021-12-15 08:38:59 -0800

    non-interactive install try 2

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ubuntu:20.04
 COPY entrypoint.sh /entrypoint.sh
+RUN DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata wget sudo gnupg
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,7 @@ APT_KEY_URL=$1
 APT_REPO_LINE=$2
 TARGET_PACKAGE=$3
 
-export DEBIAN_FRONTEND=noninteractive 
-export TZ=Etc/UTC 
-
 apt update
-apt install -y wget sudo gnupg
 
 # Install repo key
 wget -qO - "$APT_KEY_URL" | sudo apt-key add -
